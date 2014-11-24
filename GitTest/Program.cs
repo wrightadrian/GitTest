c6a1b4ec3c5c807d6e9af6dfec44371a40e81cbf
wrightadrian <wright.adrian@gmail.com>
2014-11-23 20:15:29 -0600

    Double the fun

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git World.");
+            Console.WriteLine("Hello Git World.");
         }
     }
 }
